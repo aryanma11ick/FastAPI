b67c5e95a00f137ef233f050c7f4796b109d9b95
aryanma11ick <mallickaryan1104@gmail.com>
2025-03-14 10:32:45 +0530

    adding entries in database using requests

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, Sessionlocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
